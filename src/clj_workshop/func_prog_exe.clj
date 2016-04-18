34907b11d7eec7d87397d5c83ebd7625403c3510
Artur Skowronski <a.skowronski@smartrecruiters.com>
2016-04-19 00:15:52 +0200

    2nd part of workshop

--- src/clj_workshop/func_prog_exe.clj
+++ src/clj_workshop/func_prog_exe.clj
@@ -4,75 +4,88 @@
 (defn count-chars
   "Returns count of the provided character `c` in string `s`"
   [s c]
-  (unimplemented))
+  (count (filter #(= c %) (seq s)))
+)
 
 (defn candidate-names
   [candidates]
-  (unimplemented))
+  (map #(:name %) candidates)
+)
 
 (defn candidates-with-minimum-rating
   [min-rating candidates]
-  (unimplemented))
+  (filter #(< min-rating (:rating %)) candidates)
+)
 
 (defn palindrome?
   "Checks if the given sequence is a palindrome"
   [xs]
-  (unimplemented))
+  (= (apply str (seq xs)) (apply str (reverse xs))))
 
 (defn count-palindromes
   [start end]
-  (unimplemented))
+  (count (filter palindrome? (map str (range start end)))))
+
+(defn prime? [n] (= 0 (count (filter #(= 0 (mod n %)) (range 2 n)))))
 
 (defn primes
   "Returns a lazy seq of prime numbers"
-  []
-  (unimplemented))
+	[]
+  (filter prime? (iterate inc' 2))
+)
 
 (defn flip-flop
   [nums]
-  (unimplemented))
+  (map #(if (= 0 (mod % 2)) % (+ 1 %)) nums
+))
+
 
 (defn mungle-candidate-name
   "Returns uppercased and reversed name of the candidate"
   [candidate]
-  (unimplemented))
+  (apply str (reverse (clojure.string/upper-case (:name candidate)))))
 
 (defn select-values
   "Selects keys from the provided map
   and return their values. For example:
   (select-values {:a 1 :b 2 :c 3} [:a :c]) => (1 3)"
   [map keyseq]
-  (unimplemented))
+  (clojure.core/map #(% map) keyseq))
 
 (defn rcomp
   "Composes provided functions so that
   ((rcomp f1 f2 f3) args) is the same as (f3 (f2 (f1 args)))"
   [& fns]
-  (unimplemented))
+  (comp 'fns))
 
 (defn power
   "Returns n to the power of m"
   [n m]
-  (unimplemented))
+  (reduce * (take m (repeat n))))
+
 
 (defn range-sum
   "Calculates the sum of numbers [0..n)"
   [n]
-  (unimplemented))
+  (reduce + 0 (take n (range))))
 
-(defn ratings-avg
+(defn rating-avg
   [candidates]
-  (unimplemented))
+  (/ (reduce + 0 (map #(:rating %) candidates)) (count candidates)))
 
 (defn distance-tuple
   "Calculates a distance between two points.
-  Points are two element vectors with x and y
-  coordinates."
+  Points are maps with :x and :y keys."
   [p1 p2]
-  (unimplemented))
+  (int (Math/sqrt
+   (+ (* (- (nth p1 0) (nth p2 0)) (- (nth p1 0) (nth p2 0)))
+      (* (- (nth p1 1) (nth p2 1)) (- (nth p1 1) (nth p2 1)))))))
 
 (defn distance-map
   "Calculates a distance between two points.
   Points are maps with :x and :y keys."
   [p1 p2]
-  (unimplemented))
+  (int (Math/sqrt
+   (+ (* (- (:x p1) (:x p2)) (- (:x p1) (:x p2)))
+      (* (- (:y p1) (:y p2)) (- (:y p1) (:y p2)))))))
+
